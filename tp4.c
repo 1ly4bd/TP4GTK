b37f5104501053ca92162b8708bc18a600966bbd
1ly4bd <abdulogunleye76@gmail.com>
2024-05-24 20:37:49 +0200

    messages taille memoire

--- tp4.c
+++ tp4.c
@@ -217,11 +217,11 @@ void tailleMemoire(T_Arbre abr) {
     double rapport = (1-((double)tailleIntervalles / tailleClassique)) * 100;
     append_to_message_view(g_strdup_printf("\n"));
     append_to_message_view(g_strdup_printf("La representation par intervalles occupe %.2f%% de moins que la representation classique.\n", rapport));
-    append_to_message_view(g_strdup_printf("Nombre d'octets gagnes par la representation par intervalles: %d\n", tailleClassique - tailleIntervalles));
-    append_to_message_view(g_strdup_printf("Taille memoire de l'arbre (version classique): %u octets\n", tailleClassique));
-    append_to_message_view(g_strdup_printf("Taille memoire de l'arbre (version intervalles): %u octets\n", tailleIntervalles));
-    append_to_message_view(g_strdup_printf("Taille d'un sommet par intervalle %u.\n", tailleSommetIntervalles));
-    append_to_message_view(g_strdup_printf("Taille d'un sommet normal %u.\n", tailleSommetClassique));
+    append_to_message_view(g_strdup_printf("Nombre d'octets gagnes par la representation par intervalles: %d.\n", tailleClassique - tailleIntervalles));
+    append_to_message_view(g_strdup_printf("Taille memoire de l'arbre (version classique): %u octets.\n", tailleClassique));
+    append_to_message_view(g_strdup_printf("Taille memoire de l'arbre (version intervalles): %u octets.\n", tailleIntervalles));
+    append_to_message_view(g_strdup_printf("Taille d'un sommet par intervalle: %u octets.\n", tailleSommetIntervalles));
+    append_to_message_view(g_strdup_printf("Taille d'un sommet normal: %u octets.\n", tailleSommetClassique));
 }
 
 T_Arbre obtenirRacine(T_Arbre abr) {
