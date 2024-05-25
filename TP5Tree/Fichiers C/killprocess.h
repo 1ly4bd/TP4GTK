be1e03d7c931334550785b8210d31756cfa7ebb9
1ly4bd <abdulogunleye76@gmail.com>
2024-05-25 16:39:58 +0200

    ajouté rechercherhauteur

--- TP5Tree/Fichiers C/killprocess.h
+++
@@ -1,8 +0,0 @@
-#ifndef KILLPROCESS_H
-#define KILLPROCESS_H
-
-// Déclaration du prototype de la fonction
-void killProcess();
-
-#endif /* KILLPROCESS_H */
-
--- TP5Tree/Fichiers C/message_utils.h
+++
@@ -1,9 +0,0 @@
-#ifndef MESSAGE_UTILS_H
-#define MESSAGE_UTILS_H
-
-#include <gtk/gtk.h>
-
-void append_to_message_view(const gchar *message);
-
-#endif
-
